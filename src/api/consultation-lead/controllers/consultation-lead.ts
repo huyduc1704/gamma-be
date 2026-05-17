f1c39ced90115ac8f082ad1804016834890eed9f
huyndse161632 <huyndse161632@fpt.edu.vn>
2026-05-17 16:35:21 +0700

    First commit backend Gamma Home

---
+++ src/api/consultation-lead/controllers/consultation-lead.ts
@@ -0,0 +1,7 @@
+/**
+ * consultation-lead controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::consultation-lead.consultation-lead');
---
+++ src/api/consultation-lead/routes/consultation-lead.ts
@@ -0,0 +1,7 @@
+/**
+ * consultation-lead router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::consultation-lead.consultation-lead');
